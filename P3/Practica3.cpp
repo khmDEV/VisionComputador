6df4ea69fa29af19659fc498a3a5d41c4120fd17
Aron Collados <arche193@hotmail.com>
2015-03-23 21:45:37 +0100

    Implementado Parte 1 con detector de sobel, Gradiente X, Gradiente Y … V2

--- P3/Practica3.cpp
+++ P3/Practica3.cpp
@@ -14,25 +14,20 @@
 using namespace cv;
 using namespace std;
 
+int scale = 1;
+int delta = 0;
+int ddepth = CV_32F;
+
 /*
  * Main principal
  */
 int main(int argc, char *argv[]) {
-    int MINSIZE;
-    string obj,image;
+    string image;
     if (argc == 1) {
         cout << "Introduza la ruta de la imagen;" << endl; //img/circulo2.pgm
         cin>> image;
-        cout << "Introduza el tipo de objeto" << endl;
-        cin>> obj;
-        MINSIZE = 1000;
-    } else if (argc == 4) {
-        MINSIZE = atoi(argv[4]);
-    } else if (argc < 3) {
-        cerr << " Error: nomfich nomobj [MINSIZE]" << endl;
-        return -1;
     } else {
-        MINSIZE = 1000;
+        image = argv[1];
     }
 
     Mat bgrMap = imread(image, CV_LOAD_IMAGE_COLOR); //Carga la imagen recibida por parametro
@@ -40,29 +35,64 @@ int main(int argc, char *argv[]) {
         cerr << "Could not open file " << image << endl;
         return -1;
     }
-    
-    
-    
-       namedWindow( "Original image", CV_WINDOW_AUTOSIZE );
-    imshow( "Original image", image );
- 
+
+
+
+    namedWindow("Original image", CV_WINDOW_AUTOSIZE);
+    namedWindow("Gx", CV_WINDOW_AUTOSIZE);
+    namedWindow("Magnitud del Gradiente", CV_WINDOW_AUTOSIZE);
+    imshow("Original image", bgrMap);
+
+
+    GaussianBlur(bgrMap, bgrMap, Size(3, 3), 0, 0, BORDER_DEFAULT); //Filtro Gausiano
+
     Mat grey;
-    cvtColor(image, grey, CV_BGR2GRAY);
- 
+    cvtColor(bgrMap, grey, CV_BGR2GRAY);
+
+
+    //Gradiente X////////////////////////////////////
     Mat sobelx;
-    Sobel(grey, sobelx, CV_32F, 1, 0);
- 
-    double minVal, maxVal;
-    minMaxLoc(sobelx, &minVal, &maxVal); //find minimum and maximum intensities
-    cout << "minVal : " << minVal << endl << "maxVal : " << maxVal << endl;
- 
-    Mat draw;
-    sobelx.convertTo(draw, CV_8U, 255.0/(maxVal - minVal), -minVal * 255.0/(maxVal - minVal));
- 
-    namedWindow("image", CV_WINDOW_AUTOSIZE);
-    imshow("image", draw);
- 
-    waitKey(0);                                        
+    Sobel(grey, sobelx, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT);
+
+    double minValX, maxValX;
+    minMaxLoc(sobelx, &minValX, &maxValX); //find minimum and maximum intensities
+    cout << "Gx" << "minVal : " << minValX << endl << "maxVal : " << maxValX << endl;
+
+    Mat drawX;
+    sobelx.convertTo(drawX, CV_8U, 255.0 / (maxValX - minValX), -minValX * 255.0 / (maxValX - minValX));
+
+    imshow("Gx", drawX);
+
+    //////////////////////////////////////////////////////////
+
+
+    //Gradiente Y///////////////////////////////////////
+    Mat sobely;
+    Sobel(grey, sobely, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT);
+
+    double minValY, maxValY;
+    minMaxLoc(sobely, &maxValY, &minValY); //find minimum and maximum intensities
+    cout << "Gy" << "minVal : " << minValY << endl << "maxVal : " << maxValY << endl;
+
+    Mat drawY;
+    sobely.convertTo(drawY, CV_8U, 255.0 / (maxValY - minValY), -minValY * 255.0 / (maxValY - minValY));
+
+    imshow("Gy", drawY);
+    //////////////////////////////////////////////////
+
+
+    //Magnitud del Gradiente////////////////
+    Mat grad;
+    Mat abs_grad_x, abs_grad_y;
+    convertScaleAbs(sobelx, abs_grad_x);
+    convertScaleAbs(sobely, abs_grad_y);
+    addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
+
+    imshow("Magnitud del Gradiente", grad);
+
+    /////////////////////////////////////////////////
+
+
+    waitKey(0);
     return 0;
 } 
-}
