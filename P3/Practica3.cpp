fff10d61dd8f227291ed1e217391437e08fe2e7d
khmRM <khm691@gmail.com>
2015-03-29 14:42:08 +0200

    Añadida direccion de gradiente

--- P3/Practica3.cpp
+++ P3/Practica3.cpp
@@ -48,9 +48,10 @@ int main(int argc, char *argv[]) {
 
     Mat grey;
     cvtColor(bgrMap, grey, CV_BGR2GRAY);
-
-
-    //Gradiente X////////////////////////////////////
+    grey.at<uchar>(4,4)=0;
+    grey.at<uchar>(5,5)=255;
+    grey.at<uchar>(6,6)=0;
+    //Gradiente X/////////////////////////////////////
     Mat sobelx;
     Sobel(grey, sobelx, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT);
 
@@ -62,11 +63,10 @@ int main(int argc, char *argv[]) {
     sobelx.convertTo(drawX, CV_8U, 255.0 / (maxValX - minValX), -minValX * 255.0 / (maxValX - minValX));
 
     imshow("Gx", drawX);
+    //////////////////////////////////////////////////
 
-    //////////////////////////////////////////////////////////
 
-
-    //Gradiente Y///////////////////////////////////////
+    //Gradiente Y/////////////////////////////////////
     Mat sobely;
     Sobel(grey, sobely, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT);
 
@@ -81,7 +81,7 @@ int main(int argc, char *argv[]) {
     //////////////////////////////////////////////////
 
 
-    //Magnitud del Gradiente////////////////
+    //Magnitud del Gradiente//////////////////////////
     Mat grad;
     Mat abs_grad_x, abs_grad_y;
     convertScaleAbs(sobelx, abs_grad_x);
@@ -89,10 +89,27 @@ int main(int argc, char *argv[]) {
     addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
 
     imshow("Magnitud del Gradiente", grad);
+    //////////////////////////////////////////////////
 
-    /////////////////////////////////////////////////
 
+    //Direcion del gradiente//////////////////////////
+    Mat dir=Mat::zeros(sobelx.size(),sobelx.type());
+    Mat dirS=Mat::zeros(grey.size(),grey.type());
+    float value;
 
-    waitKey(0);
+    for(int i=0;i<bgrMap.cols;i++){
+    	for(int o=0;o<bgrMap.rows;o++){
+		value=M_PI-atan2(-sobely.at<float>(i,o),-sobelx.at<float>(i,o));
+		value=value<0?0:value;  //Solucion para valores negativos muy pequeños
+		dir.at<float>(i,o)=value;
+		dirS.at<uchar>(i,o)=value/M_PI*128;
+	}
+    }
+
+    imshow("Direccion del Gradiente", dirS);
+    //////////////////////////////////////////////////
+
+    char key=0;
+    while(key!=27){key=waitKey(0);}
     return 0;
 } 
