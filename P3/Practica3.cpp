31437b67c6f91b63f38cf8ae3ba588d5e55d501e
khmRM <khm691@gmail.com>
2015-03-31 12:03:45 +0200

    Implementado calculo de punto de fuga

--- P3/Practica3.cpp
+++ P3/Practica3.cpp
@@ -17,14 +17,14 @@ using namespace std;
 int scale = 1;
 int delta = 0;
 int ddepth = CV_32F;
-
+int UMBRAL = 100;
 /*
  * Main principal
  */
 int main(int argc, char *argv[]) {
     string image;
     if (argc == 1) {
-        cout << "Introduza la ruta de la imagen;" << endl; //img/circulo2.pgm
+        cout << "Introduza la ruta de la imagen;" << endl; 
         cin>> image;
     } else {
         image = argv[1];
@@ -57,7 +57,7 @@ int main(int argc, char *argv[]) {
 
     double minValX, maxValX;
     minMaxLoc(sobelx, &minValX, &maxValX); //find minimum and maximum intensities
-    cout << "Gx" << "minVal : " << minValX << endl << "maxVal : " << maxValX << endl;
+    //cout << "Gx" << "minVal : " << minValX << endl << "maxVal : " << maxValX << endl;
 
     Mat drawX;
     sobelx.convertTo(drawX, CV_8U, 255.0 / (maxValX - minValX), -minValX * 255.0 / (maxValX - minValX));
@@ -72,7 +72,7 @@ int main(int argc, char *argv[]) {
 
     double minValY, maxValY;
     minMaxLoc(sobely, &maxValY, &minValY); //find minimum and maximum intensities
-    cout << "Gy" << "minVal : " << minValY << endl << "maxVal : " << maxValY << endl;
+    //cout << "Gy" << "minVal : " << minValY << endl << "maxVal : " << maxValY << endl;
 
     Mat drawY;
     sobely.convertTo(drawY, CV_8U, 255.0 / (maxValY - minValY), -minValY * 255.0 / (maxValY - minValY));
@@ -108,6 +108,67 @@ int main(int argc, char *argv[]) {
 
     imshow("Direccion del Gradiente", dirS);
     //////////////////////////////////////////////////
+
+
+    //Votacion al punto central///////////////////////
+    Mat center=bgrMap.clone();
+    Mat vote=bgrMap.clone();
+    int values[bgrMap.cols];
+    for(int i=0;i<bgrMap.cols;i++){
+    	values[i]=0;
+    }
+    Vec3b color=Vec3b(255,0,0);
+    float angle,xD,yD,xP,yP;
+    int mean=round(grey.rows/2); //Horizonte 
+    int direcion=0; 
+    int max=0,maxI=0;
+    for(int i=0;i<bgrMap.cols;i++){
+    	for(int o=0;o<bgrMap.rows;o++){
+    		xP=i;		 //Posicion inicial en el eje X
+		yP=o;            //Posicion inicial en el eje Y
+    		if((int)grad.at<uchar>(yP,xP)>UMBRAL){
+    			angle=dir.at<float>(yP,xP);
+			
+			xD=sin(angle);   //Modulo de la direcion en el eje X
+			yD=cos(angle);   //Modulo de la direcion en el eje Y
+			//cout<<i<<","<<o<<":- "<<xD<<" - "<<yD<<endl;
+			//cout<<i<<","<<o<<":- "<<bgrMap.cols<<" - "<<bgrMap.rows<<endl;
+			direcion=(yP<mean&&yD>0)||(yP>mean&&yD<0)?1:-1;   // direcion del vector
+			if(yD<0.99&&yD>0.01){    //Se eliminan las lineas verticales
+				while(round(yP)!=mean   //Se ha alcanzado el horizonte
+					&&(xP>=0&&xP<bgrMap.cols)&&(yP>=0&&yP<bgrMap.rows)){ 
+							//Comprueba que el punto central se encuentra en la imagen
+					center.at<Vec3b>(yP,xP)=color;
+					xP=xP+direcion*xD;
+					yP=yP+direcion*yD;
+    					//cout<<i<<","<<o<<":- "<<xP<<" - "<<yP<<endl;
+				}
+				if(xP>=0&&xP<bgrMap.cols){
+					int id=round(xP);
+					values[id]=values[id]+1;
+					if(values[id]>max){
+						max=values[id];
+						maxI=id;
+					}
+    					vote.at<Vec3b>(yP,xP)[0]=center.at<Vec3b>(xP,yP)[0]+1;
+					vote.at<Vec3b>(yP,xP)[1]=0;
+					vote.at<Vec3b>(yP,xP)[2]=0;
+				}
+			}
+    		}
+	}
+    }
+
+    for(int i=0;i<bgrMap.rows;i++){
+    	vote.at<Vec3b>(i,maxI)[0]=0;
+	vote.at<Vec3b>(i,maxI)[1]=255;
+	vote.at<Vec3b>(i,maxI)[2]=0;	
+    }
+
+    imshow("Punto central", center);
+    imshow("Punto central max", vote);
+    //////////////////////////////////////////////////
+
 
     char key=0;
     while(key!=27){key=waitKey(0);}
